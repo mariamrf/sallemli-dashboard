7c4ff70444c3db7e39e1f5f39b8aa7fdb3d88a82
Mariam Maarouf <Mariam Maarouf>
2016-02-02 12:19:19 +0200

    trying something out

--- js/script.js
+++ js/script.js
@@ -11,23 +11,37 @@
 var menuvm = new Vue({
 	el: '#top-menu',
 	data:{
+		number_of_messages: 3, //since last viewed. Last viewed is a variable with every user.
+		message_link: '#messages',
 		items: [
 		{name: 'Jane Doe', url: 'profile', icon:'fa-user'},
 		{name: 'Add Course', url:'add-course', icon: 'fa-plus-square-o'},
-		{name: 'Find Course', url:'find-course', icon: 'fa-search'},
-		{name: 'Help', url:'#help', icon: 'fa-question'}
+		{name: 'Find Course', url:'find-course', icon: 'fa-search'}//,
+		//{name: 'Help', url:'#help', icon: 'fa-question'}
 		]
+	},
+	computed: {
+		messages: function(){
+			return 'Messages (' + this.number_of_messages + ')'; 
+		}
 	}
 });
 var navmenuvm = new Vue({
 	el: '#NavMenu',
 	data:{
+		number_of_messages: 3,
+		message_link: '#messages',
 		items: [
 		{name: 'Jane Doe', url: 'profile', icon:'fa-user'},
 		{name: 'Add Course', url:'add-course', icon: 'fa-plus-square-o'},
 		{name: 'Find Course', url:'find-course', icon: 'fa-search'},
 		{name: 'Help', url:'#help', icon: 'fa-question'}
 		]
+	},
+	computed: {
+		messages: function(){
+			return 'Messages (' + this.number_of_messages + ')';
+		}
 	}
 });
 
